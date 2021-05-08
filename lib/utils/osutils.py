cca99fffc4c67d5a4b4a16a80f0968ced3567e3b
aparpara <aparpara@yandex.ru>
2021-05-08 20:11:36 +0300

    Fix link creation on Windows

--- lib/utils/osutils.py
+++ lib/utils/osutils.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import
 import os
 import errno
-
+from sys import platform
 
 def mkdir_if_missing(dir_path):
   try:
@@ -16,11 +15,15 @@ def make_symlink_if_not_exists(real_path, link_path):
   param real_path: str the path linked
   param link_path: str the path with only the symbol
   '''
-  try:
-    os.makedirs(real_path)
-  except OSError as e:
-    if e.errno != errno.EEXIST:
-      raise
+  print(link_path)
+  real_path = os.path.normpath(real_path)
+  link_path = os.path.normpath(link_path)
 
-  cmd = 'ln -s {0} {1}'.format(real_path, link_path)
-  os.system(cmd)
+  if platform == 'win32':
+    cmd = 'mklink /J "{1}" "{0}"'
+  else:
+    path_to_create = real_path
+    cmd = 'ln -s {0} {1}'
+
+  os.makedirs(real_path, exist_ok=True)
+  os.system(cmd.format(real_path, link_path))
--- main.py
+++ main.py
@@ -99,11 +99,13 @@ def main(args):
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
   else:
     torch.set_default_tensor_type('torch.FloatTensor')
-  
+
   # Redirect print to both console and log file
   if not args.evaluate:
     # make symlink
-    make_symlink_if_not_exists(osp.join(args.real_logs_dir, args.logs_dir), osp.dirname(osp.normpath(args.logs_dir)))
+    if args.real_logs_dir:
+      make_symlink_if_not_exists(osp.join(args.real_logs_dir, args.logs_dir),
+                                 osp.dirname(osp.normpath(args.logs_dir)))
     sys.stdout = Logger(osp.join(args.logs_dir, 'log.txt'))
     train_tfLogger = TFLogger(osp.join(args.logs_dir, 'train'))
     eval_tfLogger = TFLogger(osp.join(args.logs_dir, 'eval'))
@@ -120,7 +122,7 @@ def main(args):
   if args.height is None or args.width is None:
     args.height, args.width = (32, 100)
 
-  if not args.evaluate: 
+  if not args.evaluate:
     train_dataset, train_loader = \
       get_data(args.synthetic_train_data_dir, args.voc_type, args.max_len, args.num_train,
                args.height, args.width, args.batch_size, args.workers, True, args.keep_ratio)
@@ -160,7 +162,7 @@ def main(args):
     best_res = checkpoint['best_res']
     print("=> Start iters {}  best res {:.1%}"
           .format(start_iters, best_res))
-  
+
   if args.cuda:
     device = torch.device("cuda")
     model = model.to(device)
@@ -194,7 +196,7 @@ def main(args):
   loss_weights['loss_rec'] = 1.
   if args.debug:
     args.print_freq = 1
-  trainer = Trainer(model, args.evaluation_metric, args.logs_dir, 
+  trainer = Trainer(model, args.evaluation_metric, args.logs_dir,
                     iters=start_iters, best_res=best_res, grad_clip=args.grad_clip,
                     use_cuda=args.cuda, loss_weights=loss_weights)
 
@@ -205,10 +207,10 @@ def main(args):
     current_lr = optimizer.param_groups[0]['lr']
     trainer.train(epoch, train_loader, optimizer, current_lr,
                   print_freq=args.print_freq,
-                  train_tfLogger=train_tfLogger, 
+                  train_tfLogger=train_tfLogger,
                   is_debug=args.debug,
-                  evaluator=evaluator, 
-                  test_loader=test_loader, 
+                  evaluator=evaluator,
+                  test_loader=test_loader,
                   eval_tfLogger=eval_tfLogger,
                   test_dataset=test_dataset)
 
