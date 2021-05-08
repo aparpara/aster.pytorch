4f5a9be6370500d8f0ae2df09b8f78d9db802bac
aparpara <aparpara@yandex.ru>
2021-05-08 20:12:19 +0300

    Add a training script for Windows

---
+++ scripts/train.bat
@@ -0,0 +1,1 @@
+python main.py --synthetic_train_data_dir %1 --test_data_dir %2 --batch_size 128 --workers 0 --height 64 --width 256 --voc_type ALLCASES_SYMBOLS --arch ResNet_ASTER --with_lstm --logs_dir logs/%3 --real_logs_dir "" --max_len 100 --STN_ON --tps_inputsize 32 64 --tps_outputsize 32 100 --tps_margins 0.05 0.05 --stn_activation none --num_control_points 20 --print_freq 10
